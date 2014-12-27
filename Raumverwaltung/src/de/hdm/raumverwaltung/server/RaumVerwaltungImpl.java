42324441c51039bf3474e939b90480e776f17db4
Galina Schmidt <Gallerina@Gallerina-PC>
2014-12-27 22:09:25 +0100

    Eintragen in das Master

--- Raumverwaltung/src/de/hdm/raumverwaltung/server/RaumVerwaltungImpl.java
+++ Raumverwaltung/src/de/hdm/raumverwaltung/server/RaumVerwaltungImpl.java
@@ -3,24 +3,34 @@ package de.hdm.raumverwaltung.server;
 
 import java.util.ArrayList;
 import java.util.Vector;
-
 import de.hdm.raumverwaltung.server.db;
-import de.hdm.raumverwaltung.shared.*;
+import de.hdm.raumverwaltung.server.db.BenutzerMapper;
+import de.hdm.raumverwaltung.server.db.BuchungMapper;
+import de.hdm.raumverwaltung.server.db.EinladungMapper;
+import de.hdm.raumverwaltung.server.db.RaumMapper;
+
+import de.hdm.raumverwaltung.shared.Raumverwaltung;
 import de.hdm.raumverwaltung.shared.bo;
 
 import de.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class RaumverwaltungImpl extends RemoteServiceServlet implements Raumverwaltung {
 	
+	private static final long serialVersionUID = ;
+	
+	
+	private Benutzer benutzer=null;
+	private Buchung buchung=null;
+	private Einladung einladung= null;
+	private Raum raum= null;
 	
 	
 	
-	
-	
-	/**Referenzen auf die DB Mapper, die die Obejkte mit mit Server DB abgleicht
+	/**Referenzen auf die DB Mapper, die die Objekte  mit Server DB abgleicht
 	 * die Business Objekte
 	 * 
 	 */
+	
 	
 	private BenutzerMapper uMapper=null;
 	
